926c8569c5ce4a1accc2fe1746a34c344dc1780d
Juliet <musyoki.juliet011@gmail.com>
2023-09-22 12:01:52 +0300

    Done Assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+ 
+
+function returnsANamedFunction() {
+    function namedFunction() {
+      console.log('This is a named function');
+    }
+    
+    return namedFunction;
+  }
+  
+
+function returnsAnAnonymousFunction() {
+    return function() {
+      console.log('This is an anonymous function');
+    };
+  }
+  
+  
+     
+  
